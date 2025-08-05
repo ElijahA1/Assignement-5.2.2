1d3aaf5227e171cf9fb6c520f9305184cacde74c
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-05 13:42:05 -0700

    Add project files.

---
+++ Assignement 5.2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Assignement_5._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printNatNums(10, 1);
+        }
+        static void printNatNums(int n, int i) 
+        {
+
+
+            if (i <= n)
+            {
+                Console.Write($"{i} ");
+                printNatNums(n, i += 1);
+            }
+        }
+    }
+}
